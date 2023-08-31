a7536c9de15f07bd20c98b382d783ad3e748ad74
squal94 <lounis.khadir@wanadoo.fr>
2023-08-31 12:12:31 +0200

    changement de theme visuel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import bgWano from "../assets/img/BgWano.png";
 // import { useDispatch, useSelector } from "react-redux";
 // import { dataSelected } from "../features/data.slice";
 // import axios from "axios";
@@ -8,6 +9,9 @@ import Header from "../components/Header";
 const Home = () => {
   return (
     <div className="homeContainer">
+      <div className="homeContainer--bgHome">
+        <img src={bgWano} alt="Pays de Wano kuni" />
+      </div>
       <Header />
     </div>
   );
