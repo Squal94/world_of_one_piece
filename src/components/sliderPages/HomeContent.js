1f3c1e6c97c09ea25273ea7287678665ae441dae
squal94 <lounis.khadir@wanadoo.fr>
2023-09-12 14:34:01 +0200

    Debut du composant homeContent

--- src/components/sliderPages/HomeContent.js
+++ src/components/sliderPages/HomeContent.js
@@ -3,7 +3,9 @@ import React from "react";
 const HomeContent = () => {
   return (
     <div className="homeContainer">
-      <h1>home</h1>
+      <h1 className="homeContainer__title">
+        World of <img src="/assets/TitreLogo.png" alt="One piece titre" />
+      </h1>
     </div>
   );
 };
