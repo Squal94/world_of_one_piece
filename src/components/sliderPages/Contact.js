ad6b6fd973bf0d9c9b13dbe7b505793dfb32e41c
squal94 <lounis.khadir@wanadoo.fr>
2023-09-20 16:33:30 +0200

    contact fin

--- src/components/sliderPages/Contact.js
+++ src/components/sliderPages/Contact.js
@@ -4,6 +4,7 @@ const Contact = () => {
   const nameInputElement = useRef();
   const emailInputElement = useRef();
   const textAreaInputElement = useRef();
+  const formRef = useRef();
 
   const formHandler = useCallback(
     () => (event) => {
@@ -16,180 +17,89 @@ const Contact = () => {
       };
       // Possibilité de se connecter à EmailJs ou de faire un dispatch dans le store
       console.log(data);
+
+      formRef.current.reset();
     },
     []
   );
   return (
     <div className="contact" id="contact">
       <h1 className="contact--title">Signup</h1>
-      <form className="conatct__form" onSubmit={formHandler()}>
-        <div className="conatct__form--name">
-          <label htmlFor="name">Name</label>
-          <input
-            ref={nameInputElement}
-            id="name"
-            placeholder="Name"
-            type="text"
+      <div className="contact__form">
+        <img
+          className="contact__form--left"
+          src="/assets/contactLeft.png"
+          alt="Nami"
+        />
+        <form
+          className="contact__form--center"
+          ref={formRef}
+          onSubmit={formHandler()}
+        >
+          <img
+            className="contact__form--center--img1"
+            src="/assets/goodies1.png"
+            alt="luffy face 1"
           />
-        </div>
-        <div className="conatct__form--email">
-          <label htmlFor="email">Email</label>
-          <input
-            ref={emailInputElement}
-            id="email"
-            placeholder="Email"
-            type="email"
+          <img
+            className="contact__form--center--img2"
+            src="/assets/goodies2.png"
+            alt="luffy face 2"
           />
-        </div>
+          <img
+            className="contact__form--center--img3"
+            src="/assets/goodies3.png"
+            alt="luffy face 3"
+          />
 
-        <div className="conatct__form--area">
-          <label htmlFor="textArea">Message.........</label>
-          <textarea
-            ref={textAreaInputElement}
-            name="textArea"
-            id="textArea"
-            cols="30"
-            rows="10"
-          ></textarea>
-        </div>
+          <div className="contact__form--name labelForm">
+            <label htmlFor="name">
+              <h2>Name</h2>
+            </label>
+            <input
+              ref={nameInputElement}
+              id="name"
+              placeholder="Name"
+              type="text"
+            />
+          </div>
+          <div className="contact__form--email labelForm">
+            <label htmlFor="email">
+              <h2>Email</h2>
+            </label>
+            <input
+              ref={emailInputElement}
+              id="email"
+              placeholder="Email"
+              type="email"
+            />
+          </div>
 
-        <button className="conatct__form--submit" type="submit">
-          Envoyer
-        </button>
-      </form>
+          <div className="contact__form--area labelForm">
+            <label htmlFor="textArea">
+              <h2>Message :</h2>
+            </label>
+            <textarea
+              ref={textAreaInputElement}
+              placeholder="Votre message......."
+              name="textArea"
+              id="textArea"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <div className="contact__form--submit">
+            <button type="submit">Envoyer</button>
+          </div>
+        </form>
+        <img
+          className="contact__form--right"
+          src="/assets/contactRight.png"
+          alt="Robin"
+        />
+      </div>
     </div>
   );
 };
 
 export default Contact;
-
-// Contact V1 ---------------------------------------------------------------
-
-// const Contact = () => {
-//   const refContainer = useRef(null);
-//   const divContainer = useRef(null);
-
-//   const handleSubmit = (e) => {
-//     console.log(refContainer.current.value);
-//     console.log(divContainer.current);
-//   };
-//   return (
-//     <div className="contactContainer " id="contact">
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <input type="text" ref={refContainer} />
-//         </div>
-//         <div ref={divContainer}> useRef is Awesome!</div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
-// Contact V2 with bootstrap ---------------------------------------------------------------
-
-// import { useRef, useCallback } from "react";
-
-// import Card from "./components/card";
-
-// export default function App() {
-//   console.log("### Refreshing");
-
-//   const fullNameInputElement = useRef();
-//   const emailInputElement = useRef();
-//   const passwordInputElement = useRef();
-//   const passwordConfirmationInputElement = useRef();
-
-//   const formHandler = useCallback(
-//     () => (event) => {
-//       event.preventDefault();
-
-//       const data = {
-//         fullName: fullNameInputElement.current?.value,
-//         email: emailInputElement.current?.value,
-//         password: passwordInputElement.current?.value,
-//         passwordConfirmation: passwordConfirmationInputElement.current?.value
-//       };
-
-//       console.log(data);
-//     },
-//     []
-//   );
-
-//   return (
-//     <div className="flex h-screen bg-gray-500 w-screen items-center justify-center">
-//       <Card>
-//         <h1 className="text-3xl">Signup</h1>
-//         <form className="mt-4" onSubmit={formHandler()}>
-//           <div className="flex flex-col mb-4">
-//             <label
-//               className="text-gray-700 text-sm font-bold mb-2"
-//               htmlFor="full_name"
-//             >
-//               Full name
-//             </label>
-//             <input
-//               ref={fullNameInputElement}
-//               id="full_name"
-//               placeholder="Full name"
-//               type="text"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-//           <div className="flex flex-col mb-4">
-//             <label
-//               className="text-gray-700 text-sm font-bold mb-2"
-//               htmlFor="email"
-//             >
-//               Email
-//             </label>
-//             <input
-//               ref={emailInputElement}
-//               id="email"
-//               placeholder="Email"
-//               type="email"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-//           <div className="flex flex-col mb-4">
-//             <label
-//               className="text-gray-700 text-sm font-bold mb-2"
-//               htmlFor="password"
-//             >
-//               Password
-//             </label>
-//             <input
-//               ref={passwordInputElement}
-//               id="password"
-//               placeholder="Password"
-//               type="password"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-//           <div className="flex flex-col mb-4">
-//             <label
-//               className="text-gray-700 text-sm font-bold mb-2"
-//               htmlFor="password_confirmation"
-//             >
-//               Password Confirmation
-//             </label>
-//             <input
-//               ref={passwordConfirmationInputElement}
-//               id="password_confirmation"
-//               placeholder="Password Confirmation"
-//               type="password"
-//               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-//             />
-//           </div>
-//           <button
-//             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-//             type="submit"
-//           >
-//             Submit
-//           </button>
-//         </form>
-//       </Card>
-//     </div>
-//   );
-// }
