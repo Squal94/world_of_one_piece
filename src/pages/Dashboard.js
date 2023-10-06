3100f4d82585cd03312db96a54b93c218b7f0795
squal94 <lounis.khadir@wanadoo.fr>
2023-10-06 16:35:04 +0200

    probleme dashbaord affichage

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import bgWano from "../assets/img/BgWano.png";
 import { useSelector } from "react-redux";
 import { innerContent } from "../utils/functions";
+import Fruits from "../components/sliderPages/Fruits";
 
 const Dashboard = () => {
   const contentSelected = useSelector((state) => state.general.content);
+  let test = innerContent(contentSelected);
 
   return (
     <div className="dashboardContainer">
       <Header />
-      <div className="dashboardContainer__Content">
-        {innerContent(contentSelected)}
-      </div>
+      <div className="dashboardContainer__Content">{test && test}</div>
       <div className="dashboardContainer--bgHome">
         <img src={bgWano} alt="Pays de Wano kuni" />
       </div>
