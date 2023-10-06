6e750107d984e3857d37152041f095001642856f
squal94 <lounis.khadir@wanadoo.fr>
2023-10-06 16:17:55 +0200

    deploy world of one piece 12

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,16 +2,44 @@ import React from "react";
 import Header from "../components/Header";
 import bgWano from "../assets/img/BgWano.png";
 import { useSelector } from "react-redux";
-import { innerContent } from "../utils/functions";
+// import { innerContent } from "../utils/functions";
+import HomeContent from "../components/sliderPages/HomeContent";
+import Story from "../components/sliderPages/Story";
+import Equipage from "../components/sliderPages/Equipage";
+import Iles from "../components/sliderPages/Iles";
+import Fruits from "../components/sliderPages/Fruits";
+import Events from "../components/sliderPages/Events";
+import Contact from "../components/sliderPages/Contact";
 
 const Dashboard = () => {
   const contentSelected = useSelector((state) => state.general.content);
+  const innerContent = (contentSelected) => {
+    switch (contentSelected) {
+      case "home":
+        return <HomeContent />;
+      case "story":
+        return <Story />;
+      case "equipage":
+        return <Equipage />;
+      case "iles":
+        return <Iles />;
+      case "fruits":
+        return <Fruits />;
+      case "events":
+        return <Events />;
+      case "contact":
+        return <Contact />;
+      default:
+        return "";
+    }
+  };
+  // useEffect(() => {}, []);
 
   return (
     <div className="dashboardContainer">
       <Header />
       <div className="dashboardContainer__Content">
-        {contentSelected && innerContent(contentSelected)}
+        {innerContent(contentSelected)}
       </div>
       <div className="dashboardContainer--bgHome">
         <img src={bgWano} alt="Pays de Wano kuni" />
