ab7fc165011cb8c3d901b486fdadeb00d7c53aaa
squal94 <lounis.khadir@wanadoo.fr>
2023-10-06 15:54:56 +0200

    deploy world of one piece v5

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import bgWano from "../assets/img/BgWano.png";
 import { useSelector } from "react-redux";
@@ -6,13 +6,13 @@ import { innerContent } from "../utils/functions";
 
 const Dashboard = () => {
   const contentSelected = useSelector((state) => state.general.content);
+  let content = innerContent(contentSelected);
+  useEffect(() => {}, [contentSelected]);
 
   return (
     <div className="dashboardContainer">
       <Header />
-      <div className="dashboardContainer__Content">
-        {innerContent(contentSelected)}
-      </div>
+      <div className="dashboardContainer__Content">{content && content}</div>
       <div className="dashboardContainer--bgHome">
         <img src={bgWano} alt="Pays de Wano kuni" />
       </div>
