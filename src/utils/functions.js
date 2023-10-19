aad2e24c75e2a65fa21a8cb91b28efc4ac67743c
squal94 <lounis.khadir@wanadoo.fr>
2023-10-19 13:14:40 +0200

    probleme dashbaord affichage test v4

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,26 +8,24 @@ import Contact from "../components/sliderPages/Contact";
 
 export const innerContent = (contentSelected) => {
   //test 1
-  if (contentSelected) {
-    switch (contentSelected) {
-      case "home":
-        return <HomeContent />;
-      case "story":
-        return <Story />;
-      case "equipage":
-        return <Equipage />;
-      case "iles":
-        return <Iles />;
-      case "fruits":
-        return <Fruits />;
-      case "events":
-        return <Events />;
-      case "contact":
-        return <Contact />;
-      default:
-        return <HomeContent />;
-      //test 2 voir si probleme
-    }
+  switch (contentSelected) {
+    case "home":
+      return <HomeContent />;
+    case "story":
+      return <Story />;
+    case "equipage":
+      return <Equipage />;
+    case "iles":
+      return <Iles />;
+    case "fruits":
+      return <Fruits />;
+    case "events":
+      return <Events />;
+    case "contact":
+      return <Contact />;
+    default:
+      return <HomeContent />;
+    //test 2 voir si probleme
   }
 };
 
