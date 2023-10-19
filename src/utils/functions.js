160ef298fff0ab9d6cdf8882582e923fe1604cf1
squal94 <lounis.khadir@wanadoo.fr>
2023-10-19 13:20:24 +0200

    probleme dashbaord affichage test v5

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,25 +8,47 @@ import Contact from "../components/sliderPages/Contact";
 
 export const innerContent = (contentSelected) => {
   //test 1
-  switch (contentSelected) {
-    case "home":
-      return <HomeContent />;
-    case "story":
-      return <Story />;
-    case "equipage":
-      return <Equipage />;
-    case "iles":
-      return <Iles />;
-    case "fruits":
-      return <Fruits />;
-    case "events":
-      return <Events />;
-    case "contact":
-      return <Contact />;
-    default:
-      return <HomeContent />;
-    //test 2 voir si probleme
+
+  if (contentSelected === "home") {
+    return <HomeContent />;
   }
+  if (contentSelected === "story") {
+    return <Story />;
+  }
+  if (contentSelected === "equipage") {
+    return <Equipage />;
+  }
+  if (contentSelected === "iles") {
+    return <Iles />;
+  }
+  if (contentSelected === "fruits") {
+    return <Fruits />;
+  }
+  if (contentSelected === "events") {
+    return <Events />;
+  }
+  if (contentSelected === "contact") {
+    return <Contact />;
+  }
+  // switch (contentSelected) {
+  //   case "home":
+  //     return <HomeContent />;
+  //   case "story":
+  //     return <Story />;
+  //   case "equipage":
+  //     return <Equipage />;
+  //   case "iles":
+  //     return <Iles />;
+  //   case "fruits":
+  //     return <Fruits />;
+  //   case "events":
+  //     return <Events />;
+  //   case "contact":
+  //     return <Contact />;
+  //   default:
+  //     return <HomeContent />;
+  //   //test 2 voir si probleme
+  // }
 };
 
 // import axios from "axios";
